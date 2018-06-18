731633e429ca17e0db0add8d0373e5b558b62d3f
Amit Singh Sansoya <tusharamit@yahoo.com>
2018-06-19 03:01:32 +0530

    Add files via upload

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,17 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  
   public tasks:string;
   public works:any=[];
-  
+  public proedit:boolean=true;
+  public k:any;
+  public taskedit:string="";
+
   public addlist(tasks){
     this.works.push(tasks);
     console.log(this.works);
+    this.tasks="";
   }
+
   public deletelist(i){
     this.works.splice(this.works.indexOf(i),1);
   }
-  
+
+  public editlist(element){
+    this.k=this.works.indexOf(element);
+    this.proedit=!this.proedit;
+    console.log(this.k);
+  }
+
+  public makechange(taskedit1){
+    this.works[this.k]=taskedit1;
+    this.taskedit="";
+    console.log(this.taskedit)
+  }
   constructor() { }
 
   ngOnInit() {
